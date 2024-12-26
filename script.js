58390cdd184ee9d92b101884be9c9afb3e292938
BenjaminJose06 <benjaminjose2006@gmail.com>
2024-12-26 21:00:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function myfunction() {
+    if (document.getElementById('share_container').style.opacity=="1") {
+         document.getElementById('share_container').style.opacity="0";
+    } else {
+    document.getElementById('share_container').style.opacity="1";
+
+    }
+
+} 
